252c270d882c7d3a4dc05e3fc0c269011fbaff3e
mokshith13 <mokshithshettigar13@gmail.com>
2024-06-06 19:33:54 +0530

    second commit

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div>
+         <span className='mb-10 flex items-center justify-center'> &copy; 2024 AMS Estates. All Rights Reserved.</span>
+    </div>
+  )
+}

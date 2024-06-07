71350b09927dc38576cf737f0c27867f3cd187df
mokshith13 <mokshithshettigar13@gmail.com>
2024-06-07 12:00:15 +0530

    fourth commit

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -273,7 +273,7 @@ export default function CreateListing() {
                 type='number'
                 id='regularPrice'
                 min='50'
-                max='10000000'
+                max='1000000000'
                 required
                 className='p-3 border border-gray-300 rounded-lg'
                 onChange={handleChange}

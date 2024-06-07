64ab1505e078994677a2cee4e51ef479183d817a
mokshith13 <mokshithshettigar13@gmail.com>
2024-06-07 12:40:32 +0530

    fifth commit

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -5,6 +5,7 @@ export default function About() {
     <div className='py-20 px-4 max-w-6xl mx-auto'>
       <h1 className='text-3xl font-bold mb-4 text-slate-800'>About AMS Estates</h1>
       <p className='mb-4 text-slate-700'>AMS Estates is a leading real estate agency that specializes in helping clients buy, sell, and rent properties in the most desirable neighborhoods. Our team of experienced agents is dedicated to providing exceptional service and making the buying and selling process as smooth as possible.</p>
+      <p className='mb-4 text-slate-700'>Ams Estates is a cutting-edge solution designed to revolutionize the real estate industry by providing a seamless platform for managing properties, connecting buyers with sellers.</p>
      </div>
   )
 }
